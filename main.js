3d3fa987dab9853843dd1a3aaffa666961b7080e
Orel-Viktor <orelviktor93@gamil.com>
2022-10-20 19:36:52 +0300

    homework15-redy

--- main.js
+++ main.js
@@ -1,51 +1,102 @@
 "use strict";
 import "./style.css";
 
-function Validation({ input, validLeter, vallidDigit, validAmount, }) {
+function Validation({
+  input,
+  validLeter,
+  validDigit,
+  validSymbol,
+  validAmount,
+}) {
   const myInput = document.querySelector(input);
-  const inputLetter = document.querySelector(validLeter);
-  const inputDigit = document.querySelector(vallidDigit);
-  const inputAmount = document.querySelector(validAmount);
+  const letterRoot = document.querySelector(validLeter);
+  const digitRoot = document.querySelector(validDigit);
+  const symbolRoot = document.querySelector(validSymbol);
+  const amountRoot = document.querySelector(validAmount);
 
-  myInput.addEventListener("input", () => {
-    this.value = myInput.value;
-    const regEx = /^(?=.*\d)(?=.*[A-Z])[\da-zA-Z!?;:()"-.]{8}$/;
-    let res = this.value.match(regEx);
-    const regBigLetter = /(?=.*[A-Z])/
-    let resBigLetter = this.value.match(regBigLetter)
-    
-    console.log(res);
-    console.log(this.value);
-    
+  this.validation = function () {
+    myInput.addEventListener("input", () => {
+      this.value = myInput.value;
+      const regEx =
+        /^(?=.*\d)(?=.*[A-Z])(?=.*[!?;:()"-.])[\da-zA-Z!?;:()"-.]{8}$/;
+      let res = this.value.match(regEx);
+      this.initVaild();
+      if (this.value === "") {
+        myInput.classList.remove("invalid");
+        myInput.classList.remove("valid");
+        return;
+      }
+      if (!res) {
+        myInput.classList.add("invalid");
+        return;
+      }
+      if (res) {
+        myInput.classList.remove("invalid");
+        myInput.classList.add("valid");
+      }
+    });
+  };
+
+  this.initVaild = () => {
+    this.bigLetterValidation();
+    this.digitValidation();
+    this.symbolValidation();
+    this.amountValidation();
+  };
+  this.bigLetterValidation = () => {
+    const regBigLetter = /(?=.*[A-Z])/;
+    let resBigLetter = this.value.match(regBigLetter);
     if (resBigLetter) {
-      inputLetter.style.color = 'green'
-     
-    }else {
-      inputLetter.style.color = 'black'
-     
+      letterRoot.style.color = "green";
+    } else {
+      letterRoot.style.color = "black";
     }
-    if (this.value === "") {
-      myInput.classList.remove("invalid");
-      myInput.classList.remove("valid");
-      return;
-    }
-    if (!res) {
-      myInput.classList.add("invalid");
-      return;
-    }
-    if (res) {
-      myInput.classList.remove("invalid");
-      myInput.classList.add("valid");
-    }
-  });
+  };
 
+  this.digitValidation = () => {
+    const regDigit = /(?=.*\d)/;
+    let resDigit = this.value.match(regDigit);
+    if (resDigit) {
+      digitRoot.style.color = "green";
+    } else {
+      digitRoot.style.color = "black";
+    }
+  };
+
+  this.symbolValidation = () => {
+    const regSymbol = /(?=.*[!?;:()"-.])/;
+    let resSymbol = this.value.match(regSymbol);
+    if (resSymbol) {
+      symbolRoot.style.color = "green";
+    } else {
+      symbolRoot.style.color = "black";
+    }
+  };
+  this.amountValidation = () => {
+    const regAmount = /.{8}/;
+    let resAmount = this.value.match(regAmount);
+    if (resAmount) {
+      amountRoot.style.color = "green";
+    } else {
+      amountRoot.style.color = "black";
+    }
+  };
 }
 
-let inputFirst = new Validation(
-  {
-    input: '.js--input',
-    validLeter: '.js--big-letter',
-    vallidDigit: '.js--one-git',
-    validAmount: '.js--digit'
-  });
+let inputFirst = new Validation({
+  input: ".js--input",
+  validLeter: ".js--big-letter",
+  validDigit: ".js--one-digit",
+  validSymbol: ".js--symbol",
+  validAmount: ".js--amount",
+});
+inputFirst.validation();
 
+let inputSecond = new Validation({
+  input: ".js--input2",
+  validLeter: ".js--big-letter2",
+  validDigit: ".js--one-digit2",
+  validSymbol: ".js--symbol2",
+  validAmount: ".js--amount2",
+});
+inputSecond.validation();
